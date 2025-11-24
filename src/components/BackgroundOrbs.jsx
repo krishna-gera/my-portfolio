14fc9981f352f850a1808a07197884b8967ac0b5
Krishna Gera <153417466+krishna-gera@users.noreply.github.com>
2025-11-24 19:26:32 +0530

    Create BackgroundOrbs.jsx

---
+++ src/components/BackgroundOrbs.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const BackgroundOrbs = () => {
+  return (
+    <div className="orbs">
+      <div className="orb orb-1" />
+      <div className="orb orb-2" />
+      <div className="orb orb-3" />
+    </div>
+  )
+}
+
+export default BackgroundOrbs
